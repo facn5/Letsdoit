c0726f8544083002a2082e460712c1d734c8226e
Shadi Rayan <therealshadix@gmail.com>
2019-03-19 15:15:52 +0200

    Update logic.js

--- skeleton/logic.js
+++ skeleton/logic.js
@@ -28,18 +28,52 @@ var todoFunctions = {
     // returns a new array, it should contain todos with the newTodo added to the end.
     // add an id to the newTodo. You can use the generateId function to create an id.
     // hint: array.concat
+    var newid;
+    newtodos = JSON.parse(JSON.stringify(todos));
+    newid = todoFunctions.generateId();
+    newTodo.id = newid;
+
+    //  var newarr = Object.keys(newTodo).map(function(key) {
+    //    return [Number(key), newTodo[key]];
+    //  });
+    //    var newKey = { newid };
+    //  newKey.description = " description";
+    //  var newKey = Object.assign(newid, description);//
+    newtodos.concat(newTodo);
+    console.log(newtodos.concat(newTodo));
+    return newtodos;
   },
   deleteTodo: function(todos, idToDelete) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // return a new array, this should not contain any todo with an id of idToDelete
     // hint: array.filter
+    newtodos = JSON.parse(JSON.stringify(todos));
+    //  for (var i = 0; i < newtodos.length; i++) {
+    //  var obj = newtodos[idToDelete];
+    //
+    //    if (newtodos.indexOf(obj.id) !== -1) {
+    //    newtodos.splice(i, 1);
+    //  }
+    //    }
+    var sliced = Array.prototype.slice.call(newtodos, idToDelete);
+
+    //  console.log(newtodos);
+    console.log(sliced);
   },
   markTodo: function(todos, idToMark) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // in the new todo array, all elements will remain unchanged except the one with id: idToMark
     // this element will have its done value toggled
     // hint: array.map
+
+    newtodos = JSON.parse(JSON.stringify(todos));
+    //newtodos[idToMark].setAttribute("style", "background-color: red;");
+    newtodos[idToMark].done = !newtodos[idToMark].done;
+    console.log(newtodos[idToMark]);
+
+    return newtodos;
   },
+
   sortTodos: function(todos, sortFunction) {
     // stretch goal! Do this last
     // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
@@ -56,3 +90,12 @@ if (typeof module !== "undefined") {
   module.exports = todoFunctions;
 }
 console.log("the file is connected");
+
+todoFunctions.addTodo(
+  [
+    { id: -3, description: "first todo", done: false },
+    { id: -2, description: "second todo", done: false },
+    { id: -1, description: "third todo", done: false }
+  ],
+  { id: 0, description: "fourth todo", done: false }
+);
