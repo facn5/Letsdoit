79fccdb3831313151a5904148d9978c7592a51f4
Shadi Rayan <therealshadix@gmail.com>
2019-03-19 15:16:11 +0200

    Update test.js

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,6 +1,22 @@
 var test = require("tape");
 var logic = require("./logic");
 
+test("the length of addtodo after the a", function(t) {
+  var actual = logic.addTodo(
+    [
+      { id: -3, description: "first todo", done: false },
+      { id: -2, description: "second todo", done: false },
+      { id: -1, description: "third todo", done: false }
+    ],
+    { id: 0, description: "fourth todo", done: false }
+  );
+  actual = actual.length;
+  console.log(actual);
+  var expected = 4;
+  t.equal(actual, expected, "Should make the first function work");
+  t.end();
+});
+
 test("Example test", function(t) {
   t.pass();
   t.end();
